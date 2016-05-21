9ed8554b56c724a7df6b30a9bc48a7c97a26e7a4
Jan Sziegaud <j.sziegaud@gmail.com>
2016-05-21 20:08:42 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+var $searchForm = $('#search-form')
+var $searchResults = $('#search-results')
+var $searchButton = $('#searchButton')
+
+var searchString
+
+var resultDiv = '<div class="result"></div>'
+var resultTitle = '<h2 class="searchtitle"></h2>'
+var resultText = '<p></p>'
+
+function fillResults() {
+  $searchResults.children().remove();
+  json["query"]["search"].forEach(function (elem) {
+    resultTitle = '<h2 class="searchtitle">'+elem["title"]+'</h2>'
+    resultText = '<p>'+elem["snippet"]+'</p>'
+    resultDiv = '<div class="result">'+resultTitle+resultText+'</div>'
+  //elem["size"]
+    $searchResults.append(resultDiv)
+  })
+}
+
+$searchButton.on('click', function(event) {
+  event.preventDefault()
+  //var url = "https://en.wikipedia.org/w/api.php?format=json&action=query&list=search&srsearch=" + $('#search').val()
+  $.ajax( {
+    url: "http://en.wikipedia.org/w/api.php?",
+    jsonp: 'callback',
+    dataType: 'jsonp',
+    data: {
+      action: 'query',
+      props: 'info',
+      list: 'search',
+      format: 'json',
+      utf8: 1,
+      srsearch: $('#search').val()
+    },
+    xhrFields: { withCredentials: true },
+    success: function(result) {
+      $searchResults.children().remove()
+      result["query"]["search"].forEach(function (elem) {
+        resultTitle = '<h2 class="searchtitle">'+elem["title"]+'</h2>'
+        resultText = '<p>'+elem["snippet"]+'</p>'
+        resultDiv = '<div class="result material-shadow">'+resultTitle+resultText+'</div>'
+      //elem["size"]
+        $searchResults.append(resultDiv)
+      })
+    }
+  })
+})
