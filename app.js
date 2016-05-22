2e581e1a3ad362b6ef1fa0eec0cefd3fa408a2d3
Jan Sziegaud <j.sziegaud@gmail.com>
2016-05-22 09:44:45 +0200

    v1.0

--- app.js
+++ app.js
@@ -8,42 +8,36 @@ var resultDiv = '<div class="result"></div>'
 var resultTitle = '<h2 class="searchtitle"></h2>'
 var resultText = '<p></p>'
 
-function fillResults() {
-  $searchResults.children().remove();
-  json["query"]["search"].forEach(function (elem) {
-    resultTitle = '<h2 class="searchtitle">'+elem["title"]+'</h2>'
-    resultText = '<p>'+elem["snippet"]+'</p>'
-    resultDiv = '<div class="result">'+resultTitle+resultText+'</div>'
-  //elem["size"]
-    $searchResults.append(resultDiv)
-  })
-}
-
 $searchButton.on('click', function(event) {
   event.preventDefault()
-  //var url = "https://en.wikipedia.org/w/api.php?format=json&action=query&list=search&srsearch=" + $('#search').val()
   $.ajax( {
     url: "http://en.wikipedia.org/w/api.php?",
     jsonp: 'callback',
     dataType: 'jsonp',
     data: {
       action: 'query',
-      list: 'search',
+      generator: 'search',
+      gsrlimit: 10,
       format: 'json',
-      utf8: 1,
-      indexpageids:1,
-      srsearch: $('#search').val()
+      prop: 'extracts',
+      exintro: 1,
+      explaintext: 1,
+      exsentences: 1,
+      exlimit: 'max',
+      gsrsearch: $('#search').val()
     },
     xhrFields: { withCredentials: true },
     success: function(result) {
       $searchResults.children().remove()
-      result["query"]["search"].forEach(function (elem) {
-        resultTitle = '<h2 class="searchtitle">'+elem["title"]+'</h2>'
-        resultText = '<p>'+elem["snippet"]+'</p>'
-        resultDiv = '<div class="result material-shadow">'+resultTitle+resultText+'</div>'
-      //elem["size"]
-        $searchResults.append(resultDiv)
-      })
+      for(var ele in result.query.pages) {
+        console.log(ele, result.query.pages[ele])
+        resultTitle = '<h2 class="searchtitle">' + result.query.pages[ele].title + '</h2>'
+        resultText = '<p>' + result.query.pages[ele].extract + '</p>'
+        resultDiv = '<div class="result material-shadow">' + resultTitle + resultText + '</div>'
+        resultLink = '<a href="https://en.wikipedia.org/?curid=' + result.query.pages[ele].pageid + '" target="_blank">' + resultDiv + '</a>'
+        $searchResults.append(resultLink)
+      }
+
     }
   })
 })
